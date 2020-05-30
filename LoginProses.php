1f5cd7fb73d6f9beedd9ef14b6cc9b039bde2164
Nolan Tantri <ntantri@gmail.com>
2020-05-30 15:24:39 +0700

    Tambah file loginproses.php

---
+++ LoginProses.php
@@ -0,0 +1,36 @@
+<?php 
+	$koneksi = new mysqli("localhost","root","","instantgram");
+
+	$user= $_POST['username'];
+	$pass= $_POST['password'];
+
+	$sql = "SELECT * FROM user WHERE username='$user'";
+	$hasil = $koneksi->query($sql);
+
+	if ($row = $hasil->fetch_array())
+	{
+		$salt = $row['salt'];
+		$md5pass = md5($pass);
+		$md5salt= $salt.$md5pass.$salt;
+		$md5final = md5($md5salt);
+
+		if ($md5final == $row['password'])
+		{
+			echo "Sukses";
+
+			session_start();
+			$_SESSION['nama'] = $row['nama'];
+			$_SESSION['username'] = $user;
+			header("Location: FormHome.php");
+		}
+			
+		else
+		{
+			echo "<script type='text/javascript'> alert('Gagal untuk melakukan LogIn');window.location.href='FormLogIn.php'</script>";
+		}
+	}
+	else
+	{
+		echo "<script type='text/javascript'> alert('Username dan Password tidak dapat ditemukan');window.location.href='FormLogIn.php'</script>";
+	}
+ ?>
